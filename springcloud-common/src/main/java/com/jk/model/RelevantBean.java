428ab0572d2148bf29b877d426648521f1f6d74e
账号gs-cj <2469095052@qq.com>
2020-06-03 10:48:16 +0800

    添加相关职位实体类

---
+++ springcloud-common/src/main/java/com/jk/model/RelevantBean.java
@@ -0,0 +1,25 @@
+package com.jk.model;
+
+import lombok.Data;
+
+@Data
+public class RelevantBean {
+    private Integer id;
+    private String type;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
