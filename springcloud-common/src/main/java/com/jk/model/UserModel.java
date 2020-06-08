f51ddeac0d1d30c6e5073f760960822ba783b1a3
L427H <1146879277@qq.com>
2020-06-08 11:52:21 +0800

    111

---
+++ springcloud-common/src/main/java/com/jk/model/UserModel.java
@@ -0,0 +1,56 @@
+package com.ed.model;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    private Integer userid;
+
+    private String userpassword;
+
+    private String username;
+
+    private String userimage;
+
+    private String phone;
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    public String getUserpassword() {
+        return userpassword;
+    }
+
+    public void setUserpassword(String userpassword) {
+        this.userpassword = userpassword;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserimage() {
+        return userimage;
+    }
+
+    public void setUserimage(String userimage) {
+        this.userimage = userimage;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
